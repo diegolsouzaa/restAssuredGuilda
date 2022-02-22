c409dc28168344e2c18e085a62d10e99c5724179
diegolsouzaa <diegolsouzaa@gmail.com>
2022-02-22 17:46:04 -0300

    Adicionar dependencia do jackson.core

--- src/test/java/RestAssureTestOne.java
+++ src/test/java/RestAssureTestOne.java
@@ -1,6 +1,8 @@
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -13,16 +15,13 @@ public class RestAssureTestOne {
     @Test
     public void testeUm(){
 
-
         Map<String, Object> params = new HashMap<>();
         params.put("localDeEntrega","CB Barueri");
-        params.put("dataColeta","2021-08-01");
-        params.put("dataEntrega","2021-09-01");
+        params.put("dataColeta", "2022-02-01");
+        params.put("dataEntrega", "2022-02-01");
         params.put("produto","Recife");
 
-
-
-        given().log().all()
+        given()
                 .contentType(ContentType.JSON)
                 //.body("{\"localDeEntrega\" : \"Nefff\", \"dataColeta\" : \"2021-08-01\", \"dataEntrega\": \"2021-09-01\", \"produto\":\"Recife\"}")
                 .body(params)
